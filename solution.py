7a49df6cecd73ebc1360c6dcfa8926c62625c10a
chidi-udeze <only4image@yahoo.com>
2022-09-06 13:08:47 -0400

    change in code

--- solution.py
+++ solution.py
@@ -13,6 +13,7 @@ def webServer(port=13331):
 
     # Fill in start
     serverSocket.listen(1)
+    print("The server is ready to receive...")
     # Fill in end
 
     while True:
@@ -20,52 +21,48 @@ def webServer(port=13331):
 
         print("Ready to serve...")
         # Fill in start -are you accepting connections?     #Fill in end
-        connectionSocket, addr = serverSocket.accept()  
+        connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(
-                1024
-            ).decode()  # Fill in start -a client is sending you a message   #Fill in end
-            filename = message.split()[1]
+          # Fill in start -a client is sending you a message   #Fill in end
+          message = connectionSocket.recv(1024)
+          print(f"Message:\n{connectionSocket.recv(1024)}")
+          filename = message.split()[1]
 
-            # opens the client requested file.
-            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-            f = open(filename[1:])  # fill in start #fill in end)
-            # fill in end
+          # opens the client requested file.
+          # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+          f = open(filename[1:])  # fill in start #fill in end)
+          # fill in end
 
-            # outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"  # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
-            # Content-Type above is an example on how to send a header as bytes
-            outputdata = f.read()
-            f.close()
-            # Fill in end
+          # outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"  # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+          # Content-Type above is an example on how to send a header as bytes
+          outputdata = f.read()
+          # Fill in end
 
-            # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
-            # Fill in start
-            header = "HTTP/1.1 200 OK\n"
-            connectionSocket.send(header.encode())
-            # Fill in end
+          # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
+          # Fill in start
+          connectionSocket.send("\nHTTP/1.1 200 OK\n\n".encode())
+          # Fill in end
 
-            # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):  # for line in file
-                # Fill in start - send your html file contents #Fill in end
-                connectionSocket.send(outputdata[i].encode())
-            
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()  # closing the connection socket
+          # Send the content of the requested file to the client
+          for i in range(0, len(outputdata)):  # for line in file
+              # Fill in start - send your html file contents #Fill in end
+              connectionSocket.send(outputdata[i].encode())
+          connectionSocket.send("\r\n".encode())
+          connectionSocket.close()  # closing the connection socket
 
-        except IOError as e:
-            # Send response message for invalid request due to the file not being found (404)
-            # Fill in start
-            error = "HTTP/1.1 404 Not Found\n"
-            connectionSocket.send(error.encode())
-            # Fill in end
+        except IOError:
+          # Send response message for invalid request due to the file not being found (404)
+          # Fill in start
+          error = "HTTP/1.1 404 Not Found\n"
+          connectionSocket.send(error.encode())
+          # Fill in end
 
-            # Close client socket
-            # Fill in start
-            connectionSocket.close()
-            print("IO error")
-            break
-            # Fill in end
+          # Close client socket
+          # Fill in start
+          connectionSocket.close()
+          # Fill in end
+          break
 
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
