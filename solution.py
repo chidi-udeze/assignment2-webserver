e0705b9f9b1dcafe8ec313a7355feb171a1378b3
chidi-udeze <only4image@yahoo.com>
2022-09-13 23:53:32 -0400

    remove lone 20

--- solution.py
+++ solution.py
@@ -17,7 +17,7 @@ def webServer(port=13331):
           filename = message.split()[1]
           f = open(filename[1:]) 
           outputdata = f.read()
-          f.close()
+          
 
           connectionSocket.send(b'HTTP/1.0 200 OK\n')
           connectionSocket.send(b'Content-Type: text/html\n')
